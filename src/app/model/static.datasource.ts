c03e76123340cf356d5a5ae0e14e5c042eae0774
Anton Efimov <22519580+afimov@users.noreply.github.com>
2017-09-23 12:14:06 +0300

    Initial commit

---
+++ src/app/model/static.datasource.ts
@@ -0,0 +1,31 @@
+import {Injectable} from "@angular/core";
+
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/from";
+
+import {Product} from "./product.model";
+
+@Injectable()
+export class StaticDataSource {
+  private products: Product[] = [
+    new Product(1, "Product1", "Category1", "Product1 (Category1)", 100),
+    new Product(2, "Product2", "Category1", "Product2 (Category1)", 100),
+    new Product(3, "Product3", "Category1", "Product3 (Category1)", 100),
+    new Product(4, "Product4", "Category1", "Product4 (Category1)", 100),
+    new Product(5, "Product5", "Category1", "Product5 (Category1)", 100),
+    new Product(6, "Product6", "Category2", "Product6 (Category2)", 100),
+    new Product(7, "Product7", "Category2", "Product7 (Category2)", 100),
+    new Product(8, "Product8", "Category2", "Product8 (Category2)", 100),
+    new Product(9, "Product9", "Category2", "Product9 (Category2)", 100),
+    new Product(10, "Product10", "Category2", "Product10 (Category2)", 100),
+    new Product(11, "Product11", "Category3", "Product11 (Category3)", 100),
+    new Product(12, "Product12", "Category3", "Product12 (Category3)", 100),
+    new Product(13, "Product13", "Category3", "Product13 (Category3)", 100),
+    new Product(14, "Product14", "Category3", "Product14 (Category3)", 100),
+    new Product(15, "Product15", "Category3", "Product15 (Category3)", 100)
+  ];
+
+  getProducts(): Observable<Product[]> {
+    return Observable.from([this.products]);
+  }
+}
